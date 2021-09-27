a7213eab1c959c3011f426194cb31a632f86e684
兰之 <lanzhi_lee@163.com>
2021-09-27 17:09:36 +0800

    chore: 测试增加依赖

--- packages/demo1/lib/index.js
+++ packages/demo1/lib/index.js
@@ -5,4 +5,5 @@ module.exports = demo1;
 function demo1() {
     // TODO
     // some change for testing 
+    // sone change for testing axios
 }
--- packages/demo2/lib/index.js
+++ packages/demo2/lib/index.js
@@ -4,4 +4,5 @@ module.exports = demo2;
 
 function demo2() {
     // TODO
+    // sone change for testing node-fetch
 }
